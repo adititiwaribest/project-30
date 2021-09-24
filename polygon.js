4a513500a950df633400693239e11bd2c11de38a
adititiwaribest <77386139+adititiwaribest@users.noreply.github.com>
2021-09-25 05:52:15 +1400

    Add files via upload

---
+++ polygon.js
@@ -0,0 +1,13 @@
+class polygon (){
+    constructor(){
+        var options={
+            bodyA : bodyA,
+            bodyB : bodyB,
+            stiffness :0.04,
+            length : 10 ,
+        }
+     this.bodyA= bodyA;
+     this.bodyB= bodyB;
+      
+    }
+}
